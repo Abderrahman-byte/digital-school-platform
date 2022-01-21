af85baecc12351cb43c2554881c76b04e5ea6666
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-01-21 11:53:25 +0100

    Validate data add send errors

--- authentication-service/src/main/java/com/abderrahmane/elearning/authservice/controllers/RegisterController.java
+++ authentication-service/src/main/java/com/abderrahmane/elearning/authservice/controllers/RegisterController.java
@@ -1,14 +1,44 @@
 package com.abderrahmane.elearning.authservice.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.abderrahmane.elearning.authservice.utils.ErrorMessageResolver;
+import com.abderrahmane.elearning.authservice.validators.RegisterFormValidator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.validation.Errors;
+import org.springframework.validation.MapBindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/register")
+@RequestMapping("/api/auth/register")
 public class RegisterController {
-    @GetMapping
-    public String getPage () {
-        return "There is no GET for login in REST";
+    @Autowired
+    private RegisterFormValidator validator;
+
+    @Autowired
+    private ErrorMessageResolver messageResolver;
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> handlePost(@RequestBody Map<String, Object> body) {
+        MapBindingResult errors = new MapBindingResult(body, "register");
+        validator.validate(body, errors);
+
+        if (errors.hasErrors()) return constructErrorResponse(errors);
+
+        return new HashMap<String, Object>();
+    }
+
+    private Map<String, Object> constructErrorResponse(Errors errors) {
+        Map<String, Object> response = new HashMap<String, Object>();
+        response.put("errors", messageResolver.resolveField(errors));
+        response.put("ok", false);
+
+        return response;
     }
 }
