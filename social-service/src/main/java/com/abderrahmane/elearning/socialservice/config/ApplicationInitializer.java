89904d8a13884ea6dc9529287f8b26ccc4b364af
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-01-26 17:58:05 +0100

    feat : Add hibernate configration

--- social-service/src/main/java/com/abderrahmane/elearning/socialservice/config/ApplicationInitializer.java
+++ social-service/src/main/java/com/abderrahmane/elearning/socialservice/config/ApplicationInitializer.java
@@ -1,14 +1,11 @@
 package com.abderrahmane.elearning.socialservice.config;
 
-import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.RequestContextFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class ApplicationInitializer implements WebApplicationInitializer {
@@ -16,15 +13,9 @@ public class ApplicationInitializer implements WebApplicationInitializer {
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
         ctx.register(WebAppConfig.class);
-        ctx.refresh();
 
-        ServletRegistration.Dynamic dynamic = servletContext.addServlet("Dispatcher", new DispatcherServlet(ctx));
-        dynamic.addMapping("/");
-        dynamic.setLoadOnStartup(1);
-
-        FilterRegistration.Dynamic fDynamic = servletContext.addFilter("SpringMVCFilter", new RequestContextFilter());
-        fDynamic.addMappingForUrlPatterns(null, false, "/*");
-
-        servletContext.addListener(new ContextLoaderListener(ctx));
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("Dispatcher", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
     }
 }
