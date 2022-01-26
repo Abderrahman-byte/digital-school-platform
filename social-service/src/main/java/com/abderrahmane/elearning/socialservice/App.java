dff90c7d9337886b00b398e38114e2843db3c8ee
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-01-26 17:22:47 +0100

    Setup spring mvc project for social service

---
+++ social-service/src/main/java/com/abderrahmane/elearning/socialservice/App.java
@@ -0,0 +1,24 @@
+package com.abderrahmane.elearning.socialservice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.abderrahmane.elearning.socialservice.config.ApplicationInitializer;
+import com.abderrahmane.elearning.socialservice.helpers.TomcatAppLauncher;
+
+import org.springframework.web.SpringServletContainerInitializer;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        TomcatAppLauncher appLauncher = new TomcatAppLauncher();
+        Set<Class<?>> initializers = new HashSet<>();
+
+        initializers.add(ApplicationInitializer.class);
+
+        appLauncher.setPort(8080);
+        appLauncher.createWebapp();
+        appLauncher.addServletContainerInitializer(new SpringServletContainerInitializer(), initializers);
+
+        appLauncher.start();
+    }
+}
---
+++ social-service/src/main/java/com/abderrahmane/elearning/socialservice/config/WebAppConfig.java
@@ -0,0 +1,9 @@
+package com.abderrahmane.elearning.socialservice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class WebAppConfig {
+}
