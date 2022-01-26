ce6fce9a868103c1c2bc2260d67498ad3c71b6d6
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-01-26 22:06:41 +0100

    Register json type

---
+++ social-service/src/main/java/com/abderrahmane/elearning/socialservice/package-info.java
@@ -0,0 +1,7 @@
+@TypeDef(
+    name = "json", typeClass = JsonType.class
+)
+package com.abderrahmane.elearning.socialservice;
+
+import com.vladmihalcea.hibernate.type.json.JsonType;
+import org.hibernate.annotations.TypeDef;
