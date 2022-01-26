1b46abb735295170712b7d6d5f5cabaf5bffa0c4
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-01-26 09:46:20 +0100

    Feat(database): Add table of states (city->state->country)

--- database/02-profils.sql
+++ database/02-profils.sql
@@ -4,11 +4,18 @@ CREATE TABLE country (
     code VARCHAR (5) NOT NULL UNIQUE
 );
 
-CREATE TABLE city (
+CREATE TABLE "state" (
     id SERIAL PRIMARY KEY,
     name VARCHAR (100) NOT NULL,
     country_id INTEGER NOT NULL REFERENCES country (id),
     UNIQUE (country_id, name)
+);
+
+CREATE TABLE city (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (100) NOT NULL,
+    state_id INTEGER NOT NULL REFERENCES "state" (id),
+    UNIQUE (state_id, name)
 ); 
 
 CREATE TABLE IF NOT EXISTS school_profil (
