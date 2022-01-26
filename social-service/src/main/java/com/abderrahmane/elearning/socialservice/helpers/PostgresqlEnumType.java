8807394846100640403bab191e88ab723b6a25ee
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-01-26 22:03:59 +0100

    feat : Postgresql Enum type

---
+++ social-service/src/main/java/com/abderrahmane/elearning/socialservice/helpers/PostgresqlEnumType.java
@@ -0,0 +1,25 @@
+package com.abderrahmane.elearning.socialservice.helpers;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.type.EnumType;
+
+@SuppressWarnings("rawtypes")
+public class PostgresqlEnumType extends EnumType {
+    // I don't know what this method does; 
+    // just copied it from : https://thorben-janssen.com/hibernate-enum-mappings/
+
+    @Override
+    public void nullSafeSet(PreparedStatement st, Object value, int index, SharedSessionContractImplementor session)
+            throws HibernateException, SQLException {
+        if (value == null) {
+            st.setNull(index, Types.OTHER);
+        } else {
+            st.setObject(index, value.toString(), Types.OTHER);
+        }
+    }
+}
