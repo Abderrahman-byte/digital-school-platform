0a5f8370d8429d696167e9efb216943c4a4328b2
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-01-27 09:49:56 +0100

    feat : Add location entities models

---
+++ social-service/src/main/java/com/abderrahmane/elearning/socialservice/models/Country.java
@@ -0,0 +1,45 @@
+package com.abderrahmane.elearning.socialservice.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "country")
+public class Country {
+    @Id
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    public Country () {}
+
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
