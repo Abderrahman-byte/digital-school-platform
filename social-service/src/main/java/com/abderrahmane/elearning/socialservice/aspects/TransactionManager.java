5a2729c45ea5ab5c4ff109f4be5ff64609823007
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-01-28 22:48:54 +0100

    fix : set as aspect

--- social-service/src/main/java/com/abderrahmane/elearning/socialservice/aspects/TransactionManager.java
+++ social-service/src/main/java/com/abderrahmane/elearning/socialservice/aspects/TransactionManager.java
@@ -4,12 +4,14 @@ import javax.persistence.EntityManager;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Aspect
 public class TransactionManager {
     @Autowired
     private EntityManager entityManager;
