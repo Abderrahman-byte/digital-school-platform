6d784dfdfc29dd6aae04c24d2e7118cc6359fb93
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-01-28 15:50:01 +0100

    feat : add functionality for teacher profil

--- social-service/src/main/java/com/abderrahmane/elearning/socialservice/controllers/CreateProfileController.java
+++ social-service/src/main/java/com/abderrahmane/elearning/socialservice/controllers/CreateProfileController.java
@@ -11,6 +11,7 @@ import com.abderrahmane.elearning.socialservice.repositories.GeoDAO;
 import com.abderrahmane.elearning.socialservice.repositories.ProfilDAO;
 import com.abderrahmane.elearning.socialservice.utils.MessageResolver;
 import com.abderrahmane.elearning.socialservice.validators.SchoolProfilCreationValidator;
+import com.abderrahmane.elearning.socialservice.validators.TeacherProfilCreationValidator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -37,14 +38,21 @@ public class CreateProfileController {
     private SchoolProfilCreationValidator schoolProfilValidator;
 
     @Autowired
+    private TeacherProfilCreationValidator teacherProfilValidator;
+
+    @Autowired
     private MessageResolver messageResolver;
 
+    // TODO : this function must be refactored
     @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE })
     public ResponseEntity<?> handlePost(@RequestBody Map<String, Object> body, @RequestAttribute("account") Account account) {
         AccountType accountType = account.getAccountType();
 
         if (accountType.equals(AccountType.SCHOOL)) {
             Map<String, Object> responseBody = this.createSchoolProfile(body, account);
+            return new ResponseEntity<Map<String, Object>>(responseBody, HttpStatus.OK);
+        } else if (accountType.equals(AccountType.TEACHER)) {
+            Map<String, Object> responseBody = this.createTeacherProfil(body, account);
             return new ResponseEntity<Map<String, Object>>(responseBody, HttpStatus.OK);
         }
 
@@ -88,6 +96,28 @@ public class CreateProfileController {
             account, 
             location
         );
+
+        return response;
+    }
+
+    private Map<String, Object> createTeacherProfil (Map<String, Object> body, Account account) {
+        Map<String, Object> response = new HashMap<>();
+        MapBindingResult errors = new MapBindingResult(body, "teacherProfil");
+
+        response.put("ok", true);
+
+        if (account.getTeacherProfil() != null) {
+            response.put("ok", false);
+            response.put("errors", List.of("profil_already_exists"));
+            return response;
+        }
+
+        teacherProfilValidator.validate(body, errors);
+
+        if (errors.hasErrors()) return messageResolver.getErrorResponseBody(errors);
+
+        // Must create teacher profil after validation
+
 
         return response;
     }
