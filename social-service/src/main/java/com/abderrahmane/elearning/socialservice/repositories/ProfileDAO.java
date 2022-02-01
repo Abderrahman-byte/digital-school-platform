eac25112365c6cae822fad7aff06875a8f48678a
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-02-01 18:09:17 +0100

    feat : delete from teacher_school

--- social-service/src/main/java/com/abderrahmane/elearning/socialservice/repositories/ProfileDAO.java
+++ social-service/src/main/java/com/abderrahmane/elearning/socialservice/repositories/ProfileDAO.java
@@ -100,4 +100,13 @@ public class ProfileDAO {
 
         return query.executeUpdate() > 0;
     }
+
+    @WrapTransaction
+    public boolean deleteTeacherSchool (String teacherId, String schoolId) {
+        Query query = entityManager.createNativeQuery("DELETE FROM teacher_school WHERE teacher_id = ? AND school_id = ?");
+        query.setParameter(1, teacherId);
+        query.setParameter(2, schoolId);
+
+        return query.executeUpdate() > 0;
+    }
 }
