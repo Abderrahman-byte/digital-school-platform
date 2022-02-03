ecd787624380cc7a75fcd07b35992efb20888955
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-02-03 14:37:38 +0100

    feat : Make basic tornado app

--- services/admin-service/src/admin_service/app.py
+++ services/admin-service/src/admin_service/app.py
@@ -1,2 +1,24 @@
+import os
+
+import tornado.ioloop
+import tornado.web
+from dotenv import load_dotenv
+
+from admin_service.utils.settings import *
+
+class MainHandler (tornado.web.RequestHandler) :
+    def get (self):
+        self.render('index.html')
+
+def make_app ():
+    settings = get_setting()
+    
+    return tornado.web.Application([
+        (r"/", MainHandler)
+    ], **settings)
+
 def app () :
-    print("App should start here")
+    load_dotenv()
+    app = make_app()
+    app.listen(8080)
+    tornado.ioloop.IOLoop.current().start()
---
+++ services/admin-service/src/admin_service/utils/settings.py
@@ -0,0 +1,9 @@
+import os
+
+def get_setting () :
+    settings = dict()
+    settings['debug'] = True if os.getenv('debug') == 'true' else False
+    settings['cookie_secret'] = os.getenv('SECRET_KEY')
+    settings['template_path'] = os.path.realpath(os.path.join(os.path.dirname(__file__), '../../templates'))
+    
+    return settings
