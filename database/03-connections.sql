f4b86810825814863b194746c486804d0375cf61
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-03-06 17:43:04 +0100

    fix : cascade connection and request for connection tables on delete

--- database/03-connections.sql
+++ database/03-connections.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS request_for_connection (
     id VARCHAR (25) PRIMARY KEY,
-    teacher_id VARCHAR NOT NULL REFERENCES teacher_profil (account_id),
-    student_id VARCHAR NOT NULL REFERENCES student_profil (account_id),
+    teacher_id VARCHAR NOT NULL REFERENCES teacher_profil (account_id) ON DELETE CASCADE,
+    student_id VARCHAR NOT NULL REFERENCES student_profil (account_id) ON DELETE CASCADE,
     UNIQUE (teacher_id, student_id)
 );
 
 CREATE TABLE IF NOT EXISTS connection (
-    teacher_id VARCHAR NOT NULL REFERENCES teacher_profil (account_id),
-    student_id VARCHAR NOT NULL REFERENCES student_profil (account_id),
+    teacher_id VARCHAR NOT NULL REFERENCES teacher_profil (account_id) ON DELETE CASCADE,
+    student_id VARCHAR NOT NULL REFERENCES student_profil (account_id) ON DELETE CASCADE,
     
     PRIMARY KEY (teacher_id, student_id)
 );
