c17ee7575e280023b10b63fcdf82952f508adbc1
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-04-01 12:44:44 +0000

    feat: add timing of services build

--- services/start-services.sh
+++ services/start-services.sh
@@ -13,13 +13,20 @@ get_random_port () {
     echo $PORT
 }
 
+if [ "$EUID" -ne 0 ]
+then
+   echo "[ERROR] You must root to execute this script"
+   exit 1
+fi
+
 cwd=$(pwd)
 ip_address=$(hostname -I | awk '{ print $1}')
 
 # Remove run container
-docker rm -f $(docker ps -q)
+docker rm -f $(docker ps -q) 2>/dev/null 1>/dev/null
 
 # Build Auth Service
+auth_service_build_start=$(date +%s)
 docker build -t auth-service -f ./authentication-service/Dockerfile .
 
 if [ $? -ne 0 ]; then
@@ -27,7 +34,10 @@ if [ $? -ne 0 ]; then
    exit 1
 fi
 
+auth_service_build_time=$(expr $(date +%s) - $auth_service_build_start)
+
 # Start Social Service
+social_service_build_start=$(date +%s)
 docker build -t social-service -f ./social-service/Dockerfile .
 
 if [ $? -ne 0 ]; then
@@ -35,7 +45,10 @@ if [ $? -ne 0 ]; then
    exit 1
 fi
 
+social_service_build_time=$(expr $(date +%s) - $social_service_build_start)
+
 # Start School Management Service
+school_service_build_start=$(date +%s)
 docker build -t school-management -f ./school-management/Dockerfile .
 
 if [ $? -ne 0 ]; then
@@ -43,6 +56,15 @@ if [ $? -ne 0 ]; then
    exit 1
 fi
 
+school_service_build_time=$(expr $(date +%s) - $school_service_build_start)
+
 docker run -dp $get_random_port:8080 --add-host database:$ip_address auth-service
 docker run -dp $get_random_port:8080 --add-host database:$ip_address social-service
 docker run -dp $get_random_port:8080 --add-host database:$ip_address school-management
+
+echo -e "\nAuthenfication service build time : $(date -d @$auth_service_build_time +%T)"
+echo "Social service build time : $(date -d @$social_service_build_time +%T)"
+echo "School service build time : $(date -d @$school_service_build_time +%T)"
+echo "Total: $(date -d @$(expr $(expr $auth_service_build_time + $social_service_build_time) + $school_service_build_time) +%T)"
+
+cd $cwd
