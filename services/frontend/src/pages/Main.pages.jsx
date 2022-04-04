d13609f89153810e20e9b1b3e287a63b4df9b50d
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-04-04 21:26:47 +0000

    feat: add school management pages

--- services/frontend/src/pages/Main.pages.jsx
+++ services/frontend/src/pages/Main.pages.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import SchoolManagementPages from './SchoolManagement.pages'
+
+// The MainPages Component display routes specifique to each account type
 
 const MainPages = () => {
+    const { account } = useContext(AuthContext)
+
+    if (account === undefined) return <></>
+
+    if (account.accountType === 'SCHOOL') return <SchoolManagementPages />
+
     return (
         <div className='MainPages'>
             <h1>Welcome to main pages</h1>
