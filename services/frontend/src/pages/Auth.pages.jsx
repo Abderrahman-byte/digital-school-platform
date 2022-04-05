535422cdc7b6d56f2091a642682387875c5c8c27
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-04-05 20:50:38 +0000

    feat : add generic header and implement auth pages header

--- services/frontend/src/pages/Auth.pages.jsx
+++ services/frontend/src/pages/Auth.pages.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router'
+import GenericHeader from '../components/GenericHeader'
 import LoginPage from './login.page'
 import NotFoundPage from './NotFound.page'
 import RegisterPage from './Register.page'
 
+const authNavigation = [
+	{
+		content: 'login',
+		to: '/auth/login',
+		className: 'auth-btn',
+	},
+	{
+		content: 'sign up',
+		to: '/auth/sign-up',
+		className: 'auth-btn',
+	},
+]
+
 const AuthPages = () => {
     return (
         <div className='AuthPages'>
-            <header>Auth Headers</header>
-            <hr />
+            <GenericHeader navigation={authNavigation} />
+
             <Routes>
                 <Route path='' element={<Navigate to='login' />} />
                 <Route path='login' element={<LoginPage />} />
