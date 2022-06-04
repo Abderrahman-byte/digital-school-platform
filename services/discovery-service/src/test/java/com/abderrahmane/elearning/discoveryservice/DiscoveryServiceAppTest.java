9bead47e6f25ff1d58897140a8ffe9ea17747aae
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-06-04 13:27:27 +0100

    feat: add discovery service pattern

---
+++ services/discovery-service/src/test/java/com/abderrahmane/elearning/discoveryservice/DiscoveryServiceAppTest.java
@@ -0,0 +1,10 @@
+package com.abderrahmane.elearning.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DiscoveryServiceAppTest {
+    @Test
+    public void contextLoads () {}
+}
